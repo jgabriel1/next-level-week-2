ebe0af4ed38017a3e229e1c922273f6d89b1587a
jgabriel1 <josegabriel@det.ufc.br>
2020-08-03 11:59:08 -0300

    Setup application navigation.

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const TeacherList = (props: Props) => {
+    return (
+        <div>
+            <h1>TeacherList</h1>
+        </div>
+    )
+}
+
+export default TeacherList
