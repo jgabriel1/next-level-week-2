69350a18626dfd5c081d5a5c2229324227b095b0
jgabriel1 <josegabriel@det.ufc.br>
2020-08-04 14:23:24 -0300

    Created resources controllers and added cors as dependence.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
-app.get('/', (request: Request, response: Response) => {
-    return response.json({ message: 'Hello World!' })
-})
+app.use(routes)
 
 app.listen(3333)
