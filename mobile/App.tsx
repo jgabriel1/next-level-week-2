38193c79444b540015d862cec81414a9a39dc24d
jgabriel1 <josegabriel@det.ufc.br>
2020-08-06 11:17:25 -0300

    Finished external Stack navigation pages.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { AppLoading } from 'expo'
+import { StatusBar } from 'expo-status-bar'
 
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
-import { StatusBar } from 'expo-status-bar'
-
-import Landing from './src/pages/Landing'
+import AppStack from './src/routes/AppStack'
 
 export default function App() {
     let [fontsLoaded] = useFonts({
@@ -21,7 +20,7 @@ export default function App() {
     } else {
         return (
             <>
-                <Landing />
+                <AppStack />
                 <StatusBar style='light' />
             </>
         )
